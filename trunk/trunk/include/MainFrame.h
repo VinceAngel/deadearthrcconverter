50beff878130de1190eb65ded7f3b915524d309f
ange3d <ange3d@0dc361ce-3355-11de-8ef0-0f53c1b7e458>
2009-04-28 21:23:03 +0000

    Base du code

---
+++ trunk/trunk/include/MainFrame.h
@@ -0,0 +1,35 @@
+#ifndef _MAIN_FRAME_H_
+#define _MAIN_FRAME_H_
+
+#include <wx/wx.h>
+
+
+enum
+{
+	ID_Quit=1,
+	ID_About,
+	ID_Clic,
+	ID_In,
+	ID_Out
+};
+
+
+class MainFrame : public wxFrame
+{
+public:
+	MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+/*	void OnQuit(wxCommandEvent& event);
+	void OnAbout(wxCommandEvent& event);*/
+	void Clic(wxCommandEvent& event);
+	DECLARE_EVENT_TABLE()
+
+protected :
+
+	wxPanel * panel;
+	wxTextCtrl* inputText;
+	wxTextCtrl* outputText;
+	wxButton* button;
+};
+
+#endif //_MAIN_FRAME_H_
+
