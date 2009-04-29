bff3d394182067bd7aa02b56697d6bfd33a1fcfa
ange3d <ange3d@0dc361ce-3355-11de-8ef0-0f53c1b7e458>
2009-04-29 19:03:22 +0000

    Base for parsing

--- trunk/include/Parser.h
+++ trunk/include/Parser.h
@@ -2,18 +2,72 @@
 #define _PARSER_H_
 
 #include <string>
+using std::string;
 
 class Parser
 {
 
 public :
-	Parser(std::string input);
-	~PArser();
+	Parser(string input);
+	~Parser();
 
 	void readAndParse();
 	void compil();
-	std::string getOutput();
+	string getOutput();
 
+	//bonus attaquant
+	struct sBonus
+	{
+		int race;
+		int general;
+		int defense;
+		int formation;
+	};
+
+	//troupes
+	struct sTroupe
+	{
+		unsigned int t1;
+		unsigned int t2;
+		unsigned int t3;
+		unsigned int t4;
+		unsigned int defenses;
+	};
+
+	struct sGains
+	{
+		unsigned long bois;
+		unsigned long pierre;
+		unsigned long brique;
+		unsigned long fer;
+		unsigned long eau;
+		unsigned long nourriture;
+		unsigned long or;
+		unsigned long hectares;
+	};
+
+private:
+
+	//nom de l'adversaire
+	string name;
+	//date
+	string date;
+	//mode
+	string mode;
+	//bonus
+	sBonus bonus;
+	//troupes du joueur 1 avant le combat
+	sTroupe troupesBefore1;
+	//troupes du joueur 2 avant le combat
+	sTroupe troupesBefore2;
+	//troupes du joueur 1 apres le combat
+	sTroupe troupesAfter1;
+	//troupes du joueur 2 apres le combat
+	sTroupe troupesAfter2;
+	//gains joueur 1
+	sGains gains1;
+	//gains joueur 2
+	sGains gain2;	
 
 };
 
