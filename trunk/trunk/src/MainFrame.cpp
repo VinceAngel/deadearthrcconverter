25435a2d8cf22f73f22a59a251e934c52ad370ed
ange3d <ange3d@0dc361ce-3355-11de-8ef0-0f53c1b7e458>
2009-05-04 19:09:08 +0000

    Issue 3 done.

--- trunk/trunk/src/MainFrame.cpp
+++ trunk/trunk/src/MainFrame.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 
+#include <wx/clipbrd.h>
+
 MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
 	: wxFrame((wxFrame*)NULL,-1,title,pos,size, wxMINIMIZE_BOX | wxSYSTEM_MENU | wxCAPTION | wxCLOSE_BOX | wxCLIP_CHILDREN)
 {
@@ -19,6 +21,8 @@ MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& si
 
 	//button
 	button = new wxButton(panel, ID_Clic, "Convertir",wxPoint(360,100),wxSize(80,40));
+	bPaste = new wxButton(panel, ID_Paste, "Coller",wxPoint(130,312),wxSize(70,30));
+	bCopy = new wxButton(panel, ID_Copy, "Copier",wxPoint(600,312),wxSize(70,30));
 
 	// input text
 	inputText = new wxTextCtrl(panel, ID_In, "Collez votre RC ici", wxPoint(2,2), wxSize(350,309),wxTE_MULTILINE);
@@ -27,6 +31,7 @@ MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& si
 	// output text
 	outputText = new wxTextCtrl(panel, ID_Out, "", wxPoint(448,2), wxSize(348,309),wxTE_READONLY | wxTE_MULTILINE);
 	outputText->SetHelpText("Get you RC here");
+
 	
 }
 
@@ -52,6 +57,37 @@ void MainFrame::Clic(wxCommandEvent &event)
 	SetStatusText(p.getErrorString());
 }
 
+void MainFrame::Copy(wxCommandEvent &event)
+{
+	if (wxTheClipboard->Open())
+	{
+		// This data objects are held by the clipboard, 
+		// so do not delete them in the app.
+		wxTheClipboard->SetData( new wxTextDataObject(outputText->GetValue()) );
+		wxTheClipboard->Close();
+	}
+}
+
+
+void MainFrame::Paste(wxCommandEvent &event)
+{
+	if (wxTheClipboard->Open())
+	{
+		if (wxTheClipboard->IsSupported( wxDF_TEXT ))
+		{
+			wxTextDataObject data;
+			wxTheClipboard->GetData( data );
+			inputText->SetValue(data.GetText());
+		}  
+		wxTheClipboard->Close();
+	}
+
+}
+
+
+
 BEGIN_EVENT_TABLE(MainFrame,wxFrame)
     EVT_BUTTON(ID_Clic,MainFrame::Clic)
+    EVT_BUTTON(ID_Paste,MainFrame::Paste)
+    EVT_BUTTON(ID_Copy,MainFrame::Copy)
 END_EVENT_TABLE()
